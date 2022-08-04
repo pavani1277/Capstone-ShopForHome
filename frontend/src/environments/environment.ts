4178968a8ea2f80a4b5b3334ac75a5b9c7c72a8d
pavani1277 <85681249+pavani1277@users.noreply.github.com>
2022-08-04 23:08:22 +0530

    updated

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false
 };
-export const apiUrl = '//54.165.196.87:8080';
+export const apiUrl = '//18.232.63.23:8080';
 
 /*
  * For easier debugging in development mode, you can import the following file
